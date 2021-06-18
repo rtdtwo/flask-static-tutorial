aa8988b7ee26bcdeabf89fc3ab31bf149ed3743e
Rishabh Tatiraju <me@rishabh.blog>
2021-06-18 22:46:03 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__, static_folder='static', static_url_path='')
+
+
+@app.route('/galaxy')
+def galaxy():
+    return app.send_static_file('galaxy.html')
+
+
+if __name__ == '__main__':
+    app.run()
